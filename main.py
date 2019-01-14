42b9403dc641ab1643a8ca2c3e8f256aa056a6af
pgcrobinson <42620748+pgcrobinson@users.noreply.github.com>
2019-01-14 15:43:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ def hello_world():
 
 
 if __name__ =='__main__':
-    HOST = os.environ.get('SERVER_HOST','localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT','5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run()
+#     HOST = os.environ.get('SERVER_HOST','localhost')
+#     try:
+#         PORT = int(os.environ.get('SERVER_PORT','5555'))
+#     except ValueError:
+#         PORT = 5555
+#     app.run(HOST, PORT)
+
